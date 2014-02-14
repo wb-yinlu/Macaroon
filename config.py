6287edc2c09c5f6dbc9e471340ecb7d4509b464b
wb-yinlu_0 <wb-yinlu@v125201083.bja>
2014-02-14 12:02:26 +0800

    yl case update

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 """ all global config """
 
-_server = '192.168.56.93'
+
+_server = '10.125.202.112'
+#_server = '10.125.9.41'
 _port = 8080
+
 _host = 'macaroon.zymlinux.net'
 _timeout = 15
-_msport = 8192 
+_msport = 8192
 
 #log config, True - print, False - no print
 _debug = True
@@ -14,13 +17,11 @@ _agentport = 8192
 _agentserver = 'http://' + _server + ':' + str(_agentport)
 
 #DUT related cmd
-
+'''
+DUT_Start = '/sbin/service swift start'
+DUT_Stop = '/sbin/service swift stop'
+DUT_Clean = 'rm /home/cdn/swift/sht_filename'
+'''
 DUT_Start = '/etc/init.d/trafficserver start'
 DUT_Stop = '/etc/init.d/trafficserver stop'
 DUT_Clean = 'traffic_server -Cclear'
-
-'''
-DUT_Start = '/usr/local/squid/sbin/squid -s'
-DUT_Stop = '/usr/local/squid/sbin/squid -k shutdown'
-DUT_Clean = 'rm /usr/local/squid/var/cache/00/00/*'
-'''
